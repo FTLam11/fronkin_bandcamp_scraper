38b7b51f080d21a743f41c316ac2e789b823b225
Fronk Lom Lom <ryzingsun11@yahoo.com>
2018-06-01 18:14:42 +0800

    Scrape release title and artist name

--- lib/fronkin_bandcamp_scraper/scraper.rb
+++ lib/fronkin_bandcamp_scraper/scraper.rb
@@ -3,10 +3,18 @@ require 'nokogiri'
 
 module FronkinBandcampScraper
   class Scraper
-    attr_reader :doc
+    attr_reader :doc, :release_title, :artist_name
 
     def initialize(url)
       @doc = Nokogiri::HTML(open(url)) { |config| config.noblanks }
+      scrape
+    end
+
+    private
+
+    def scrape
+      @release_title = doc.css('h2.trackTitle').text.strip
+      @artist_name = doc.css('span.title').text
     end
   end
 end
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,7 +1,14 @@
 RSpec.describe FronkinBandcampScraper::Scraper do
-  it 'scrapes Bandcamp album URLs' do
-    url = 'https://piglet.bandcamp.com/album/lava-land'
-    scraper = FronkinBandcampScraper::Scraper.new(url)
-    expect(scraper.doc.class).to be Nokogiri::HTML::Document
+  context 'with a Bandcamp release URL' do
+    let(:url) { 'https://piglet.bandcamp.com/album/lava-land' }
+    let(:scraper) { FronkinBandcampScraper::Scraper.new(url) }
+
+    it 'scrapes the release title' do
+      expect(scraper.release_title).to eq 'lava land'
+    end
+
+    it 'scrapes the artist name' do
+      expect(scraper.artist_name).to eq 'piglet'
+    end
   end
 end
