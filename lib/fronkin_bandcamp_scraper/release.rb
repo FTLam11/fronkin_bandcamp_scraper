a3010eeddb33c39d1b446cba7e8c7314845da991
Fronk Lom Lom <ryzingsun11@yahoo.com>
2018-07-09 14:13:53 +0800

    Scrape formats for a release

--- lib/fronkin_bandcamp_scraper/release.rb
+++ lib/fronkin_bandcamp_scraper/release.rb
@@ -1,4 +1,5 @@
 require 'fronkin_bandcamp_scraper/track'
+require 'fronkin_bandcamp_scraper/format'
 
 module FronkinBandcampScraper
   class Release
@@ -11,6 +12,7 @@ module FronkinBandcampScraper
       @cover = doc.css('div#tralbumArt a.popupImage').attribute('href').value
       @tracks = scrape_tracks
       @tags = doc.css('div.tralbumData.tralbum-tags a.tag').map(&:text)
+      @formats = scrape_formats
     end
 
     private
@@ -19,6 +21,11 @@ module FronkinBandcampScraper
       track_titles = doc.css('table#track_table td.title-col span[itemprop="name"]').map(&:text)
       track_durations = doc.css('table#track_table td.title-col span.time').map { |node| node.text.strip }
       track_titles.zip(track_durations).map.with_index { |t, idx| Track.new(idx + 1, t[0], t[1]) }
+    end
+
+    def scrape_formats
+      format_choices = doc.css('li.buyItem')
+      format_choices.map { |choice| Format.new(choice) }
     end
   end
 end
