d9c8a09e1bed0fef052e055b46c837d0aa5733ed
Fronk Lom Lom <ryzingsun11@yahoo.com>
2018-07-09 14:13:36 +0800

    Add available attribute to format

--- lib/fronkin_bandcamp_scraper/format.rb
+++ lib/fronkin_bandcamp_scraper/format.rb
@@ -1,13 +1,14 @@
 module FronkinBandcampScraper
   class Format
-    attr_accessor :name, :items, :description, :product_images, :notes
+    attr_accessor :name, :items, :description, :product_images, :notes, :available
 
     def initialize(choice)
       @name = choice.css('h3.hd span').text.strip
       @items = choice.css('h3.hd div.secondaryText').text.strip
-      @description = choice.css('div.bd').text.strip
+      @description = choice.css('div.bd').text.strip.gsub(/\n\s+/, ' ')
       @product_images = choice.css('a.popupImage').map { |anchor| anchor.attribute('href').value }
-      @notes = choice.css('div.ft').text.strip
+      @notes = choice.css('div.ft h4.main-button').text.strip.gsub(/\n\s+/, ' ')
+      @available = choice.css('div.ft h4.notable').text.strip != 'Sold Out' ? true : false
     end
   end
 end
