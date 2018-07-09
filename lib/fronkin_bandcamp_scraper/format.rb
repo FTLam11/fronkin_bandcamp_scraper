27ef4194a88b58d2eed8a55e7b31ce2a5870068e
Fronk Lom Lom <ryzingsun11@yahoo.com>
2018-07-09 23:41:02 +0800

    Add sanitizer method and use better names

--- lib/fronkin_bandcamp_scraper/format.rb
+++ lib/fronkin_bandcamp_scraper/format.rb
@@ -1,14 +1,20 @@
 module FronkinBandcampScraper
   class Format
-    attr_accessor :name, :items, :description, :product_images, :notes, :available
+    attr_accessor :name, :product, :description, :product_images, :price, :is_available
 
     def initialize(choice)
-      @name = choice.css('h3.hd span').text.strip
-      @items = choice.css('h3.hd div.secondaryText').text.strip
-      @description = choice.css('div.bd').text.strip.gsub(/\n\s+/, ' ')
+      @name = sanitize(choice.css('h3.hd span').text) || 'Subcription'
+      @product = choice.css('h3.hd div.secondaryText').text.strip
+      @description = sanitize(choice.css('div.bd').text)
       @product_images = choice.css('a.popupImage').map { |anchor| anchor.attribute('href').value }
-      @notes = choice.css('div.ft h4.main-button').text.strip.gsub(/\n\s+/, ' ')
-      @available = choice.css('div.ft h4.notable').text.strip != 'Sold Out' ? true : false
+      @price = sanitize(choice.css('div.ft h4.main-button').text) || sanitize(choice.css('h4.ft.compound-button').text)
+      @is_available = choice.css('div.ft h4.notable').text.strip != 'Sold Out' ? true : false
+    end
+
+    private
+
+    def sanitize(text)
+      text.strip.gsub!(/\n\s+/, ' ')
     end
   end
 end
