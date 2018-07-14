2fbf1f0f13d600b38505f514ec86d546d94a0101
Fronk Lom Lom <ryzingsun11@yahoo.com>
2018-07-14 09:55:09 +0800

    Use refinements to clean up Format

--- lib/fronkin_bandcamp/format.rb
+++ lib/fronkin_bandcamp/format.rb
@@ -1,20 +1,24 @@
 module FronkinBandcamp
+  module CleanAsFuck
+    refine String do
+      def kleanse
+        strip.gsub!(/\n\s+/, ' ')
+      end
+    end
+  end
+
+  using CleanAsFuck
+
   class Format
     attr_accessor :name, :product, :description, :product_images, :price, :is_available
 
     def initialize(choice)
-      @name = sanitize(choice.css('h3.hd span').text) || 'Subcription'
+      @name = choice.css('h3.hd span').text.kleanse || 'Subcription'
       @product = choice.css('h3.hd div.secondaryText').text.strip
-      @description = sanitize(choice.css('div.bd').text)
+      @description = choice.css('div.bd').text.kleanse
       @product_images = choice.css('a.popupImage').map { |anchor| anchor.attribute('href').value }
-      @price = sanitize(choice.css('div.ft h4.main-button').text) || sanitize(choice.css('h4.ft.compound-button').text)
+      @price = choice.css('div.ft h4.main-button').text.kleanse || choice.css('h4.ft.compound-button').text.kleanse
       @is_available = choice.css('div.ft h4.notable').text.strip != 'Sold Out' ? true : false
-    end
-
-    private
-
-    def sanitize(text)
-      text.strip.gsub!(/\n\s+/, ' ')
     end
   end
 end
