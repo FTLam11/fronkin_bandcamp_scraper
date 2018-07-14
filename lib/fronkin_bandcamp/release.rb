cfc4726cddf20bf15ef4a965b017642ced349775
Fronk Lom Lom <ryzingsun11@yahoo.com>
2018-07-14 10:05:51 +0800

    Remove giant Nokogiri doc from ivars

--- lib/fronkin_bandcamp/release.rb
+++ lib/fronkin_bandcamp/release.rb
@@ -3,27 +3,26 @@ require 'fronkin_bandcamp/format'
 
 module FronkinBandcamp
   class Release
-    attr_accessor :doc, :title, :date, :cover, :tracks, :tags, :formats, :fans
+    attr_reader :title, :date, :cover, :tracks, :tags, :formats, :fans
 
     def initialize(doc)
-      @doc = doc
       @title = doc.css('div#name-section h2.trackTitle').text.strip
       @date = doc.css('div.tralbumData.tralbum-credits meta').attribute('content').value
       @cover = doc.css('div#tralbumArt a.popupImage').attribute('href').value
-      @tracks = scrape_tracks
+      @tracks = scrape_tracks(doc)
       @tags = doc.css('div.tralbumData.tralbum-tags a.tag').map(&:text)
-      @formats = scrape_formats
+      @formats = scrape_formats(doc)
     end
 
     private
 
-    def scrape_tracks
+    def scrape_tracks(doc)
       track_titles = doc.css('table#track_table td.title-col span[itemprop="name"]').map(&:text)
       track_durations = doc.css('table#track_table td.title-col span.time').map { |node| node.text.strip }
       track_titles.zip(track_durations).map.with_index { |t, idx| Track.new(idx + 1, t[0], t[1]) }
     end
 
-    def scrape_formats
+    def scrape_formats(doc)
       format_choices = doc.css('li.buyItem')
       format_choices.map { |choice| Format.new(choice) }
     end
