f5d398c3c24c18fba8f898ae05c96d0905eea9eb
Frank Lam <ryzingsun11@yahoo.com>
2024-08-12 01:22:57 +0900

    Fix specs
    Original release got taken down

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,21 +1,21 @@
 RSpec.describe FronkinBandcamp::Scraper do
   context 'with a Bandcamp release URL' do
-    let(:url) { 'https://piglet.bandcamp.com/album/lava-land' }
+    let(:url) { 'https://doncaballero.bandcamp.com/album/what-burns-never-returns' }
     let(:scraper) { FronkinBandcamp::Scraper.new(url) }
 
     context 'for a release' do
       subject(:release) { scraper.release }
 
       it 'scrapes the title' do
-        expect(release.title).to eq 'lava land'
+        expect(release.title).to eq 'What Burns Never Returns'
       end
 
       it 'scrapes the date' do
-        expect(release.date).to eq '1/1/2005'
+        expect(release.date).to eq '6/9/1998'
       end
 
       it 'scrapes the cover' do
-        expect(release.cover).to eq 'https://f4.bcbits.com/img/a0461781564_10.jpg'
+        expect(release.cover).to eq 'https://f4.bcbits.com/img/a0238936032_10.jpg'
       end
 
       it 'scrapes the formats' do
@@ -31,7 +31,7 @@ RSpec.describe FronkinBandcamp::Scraper do
       end
 
       it 'scrapes the album ID' do
-        expect(release.bandcamp_album_id).to eq '1361123475'
+        expect(release.bandcamp_album_id).to eq '3437957695'
       end
 
       it 'scrapes the release ID' do
@@ -41,34 +41,54 @@ RSpec.describe FronkinBandcamp::Scraper do
       subject(:tracks) { release.tracks }
 
       it 'scrapes the track numbers' do
-        expect(tracks.map(&:number)).to eq (1..6).to_a
+        expect(tracks.map(&:number)).to eq (1..8).to_a
       end
 
       it 'scrapes the track titles' do
-        expect(tracks.map(&:title)).to include 'bug stomp'
+        expect(tracks.map(&:title)).to contain_exactly(
+          "Don Caballero 3",
+          "In the Absence of Strong Evidence to the Contrary, One May Step Out of the Way of the Charging Bull",
+          "Delivering the Groceries at 138 Beats per Minute",
+          "Slice Where You Live Like Pie",
+          "Room Temperature Suite",
+          "The World in Perforated Lines",
+          "From the Desk of Elsewhere Go",
+          "June Is Finally Here"
+        )
       end
 
       it 'scrapes the track durations' do
-        bug_stomp = tracks.first
-        expect(bug_stomp.duration).to eq '03:36'
+        expect(tracks.map(&:duration)).to contain_exactly(
+          "09:42", "04:35", "05:49", "05:09", "05:31", "03:52", "07:51", "04:56"
+        )
       end
 
       it 'scrapes the tags' do
-        expect(release.tags).to include 'math rock'
+        expect(release.tags).to contain_exactly(
+          "alternative rock",
+          "instrumental",
+          "metal",
+          "experimental",
+          "instrumental",
+          "math rock",
+          "post-rock",
+          "progressive rock",
+          "Pittsburgh"
+        )
       end
     end
 
     context 'for an artist' do
       it 'scrapes the name' do
-        expect(scraper.artist.name).to eq 'piglet'
+        expect(scraper.artist.name).to eq 'Don Caballero'
       end
 
       it 'scrapes the photo' do
-        expect(scraper.artist.photo).to eq 'https://f4.bcbits.com/img/0003586442_10.jpg'
+        expect(scraper.artist.photo).to eq 'https://f4.bcbits.com/img/0000083964_10.jpg'
       end
 
       it 'scrapes the location' do
-        expect(scraper.artist.location).to eq 'Chicago, Illinois'
+        expect(scraper.artist.location).to eq 'Pittsburgh, Pennsylvania'
       end
     end
   end
